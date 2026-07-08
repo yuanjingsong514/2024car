7b942fb3fed2663dba51997b3062c22b93762c25
yuanjingsong514 <yuanjingsong514@users.noreply.github.com>
2026-07-08 20:57:25 +0800

    tune: RX计数每3秒输出一次, 显示增量

--- main.c
+++ main.c
@@ -33,16 +33,23 @@ int main(void)
     for (int i = 0; b[i]; i++)
         DL_UART_Main_transmitDataBlocking(PID_UART_INST, (uint8_t)b[i]);
 
+    uint16_t last_rx = 0;
     while (1) {
-        delay_cycles(CPUCLK_FREQ);  /* 1秒发一次计数 */
-        char buf[20];
+        delay_cycles(CPUCLK_FREQ * 3);  /* 3秒发一次 */
         uint16_t r = g_rx_count;
-        buf[0] = 'R'; buf[1] = 'X'; buf[2] = '='; buf[3] = '0' + (r / 10000) % 10;
-        buf[4] = '0' + (r / 1000) % 10; buf[5] = '0' + (r / 100) % 10;
-        buf[6] = '0' + (r / 10) % 10; buf[7] = '0' + (r % 10);
-        buf[8] = '\r'; buf[9] = '\n';
+        char buf[30];
+        buf[0] = 'R'; buf[1] = 'X'; buf[2] = '=';
+        buf[3] = '0' + (r / 10000) % 10; buf[4] = '0' + (r / 1000) % 10;
+        buf[5] = '0' + (r / 100) % 10; buf[6] = '0' + (r / 10) % 10;
+        buf[7] = '0' + (r % 10);
+        buf[8] = ' '; buf[9] = '+'; buf[10] = '=';
+        uint16_t d = r - last_rx; last_rx = r;
+        buf[11] = '0' + (d / 100) % 10;
+        buf[12] = '0' + (d / 10) % 10; buf[13] = '0' + (d % 10);
+        buf[14] = '/'; buf[15] = '3'; buf[16] = 's';
+        buf[17] = '\r'; buf[18] = '\n';
         int j;
-        for (j = 0; j < 10; j++)
+        for (j = 0; j < 19; j++)
             DL_UART_Main_transmitDataBlocking(PID_UART_INST, (uint8_t)buf[j]);
     }
 }
